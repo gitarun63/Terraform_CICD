b955b19e131c878d5c6f92ed390345cafb24dead
gitarun63 <169031084+gitarun63@users.noreply.github.com>
2024-05-27 14:20:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0440d3b780d96b29d"
+    ami = "ami-02bf8ce06a8ed6092"
     instance_type = "t2.nano"
     tags = {
       Name = "dev-ec2"
